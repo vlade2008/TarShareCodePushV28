db8132ce05838d1b013af8839efa77ed98be3b42
Christian John Saclao <vlade1356@gmail.com>
2016-08-05 16:46:45 +0800

    Upload file

---
+++ android/app/src/main/java/com/tarsharecodepushv28/MainActivity.java
@@ -0,0 +1,50 @@
+package com.tarsharecodepushv28;
+
+import com.facebook.react.ReactActivity;
+import com.microsoft.codepush.react.CodePush;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.microsoft.codepush.react.CodePush;
+
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "TarShareCodePushV28";
+    }
+
+    @Override
+    protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+    }
+
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new CodePush("2xsxtR10y2artTDQWAL5vCklwqib41SLmasub", this, BuildConfig.DEBUG)
+        );
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import codePush from "react-native-code-push";
+
+class TarShareCodePushV28 extends Component {
+
+  componentDidMount(){
+    codePush.sync({ updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+        sample
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TarShareCodePushV28', () => TarShareCodePushV28);
